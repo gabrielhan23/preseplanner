b750dcfeb252c2cc725da7477332b2daf15861a4
gabrielhan23 <69331301+gabrielhan23@users.noreply.github.com>
2020-12-27 00:20:57 -0800

    Add files via upload

---
+++ otherFiles/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+from otherFiles.setup import *
+from otherFiles.databases import *
+
+class RegistrationForm(FlaskForm):
+    fname = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+    lname = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
