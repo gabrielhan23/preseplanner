e97dd9f34928580aeb120534d6aa0a0c6551706c
gabrielhan23 <69331301+gabrielhan23@users.noreply.github.com>
2020-12-27 00:31:05 -0800

    Create group.js

---
+++ otherFiles/static/js/group.js
@@ -0,0 +1,7 @@
+function openPerson(id, group){
+  window.location.href=("http://wishlist1.1234567890hihi.repl.co/person/"+group+"/"+id)
+}
+
+function goBack(){
+  window.location.href = "http://wishlist1.1234567890hihi.repl.co/groups"
+}
